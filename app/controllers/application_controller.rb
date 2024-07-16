27d95afb4a464006e0e387e46f51e3f92b5b0c3c
Ronak Gupta <iamronakgupta@gmail.com>
2024-07-16 20:39:22 +0530

    authorize_user method added to authenticate user on tokens

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::API
+  before_action :authenticate_user!
+
+  def authenticate_user!
+    header = request.headers['Authorization']
+    header = header.split(' ').last if header
+    decoded = JsonWebToken.decode(header)
+    @current_user = User.find(decoded[:user_id]) if decoded
+  rescue ActiveRecord::RecordNotFound, JWT::DecodeError
+    render json: { errors: 'Unauthorized' }, status: :unauthorized
+  end
 end
