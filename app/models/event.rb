5d996721af4e4a233a9f33166d139bf56b816c4f
Ronak Gupta <iamronakgupta@gmail.com>
2024-07-18 12:31:59 +0530

    event validation addeD

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,10 @@
 class Event < ApplicationRecord
-
+  # Validation
+  validates :name, :description, :start_date, :start_time, :end_time, presence: true
+  validates :recurrence, inclusion: { in: [ true, false ] }
+  validates :recurrence_week, :repeat_days, presence: true, if: -> {recurrence == true}
   # Association
   has_and_belongs_to_many :employees, class_name: "User"
+
+
 end
